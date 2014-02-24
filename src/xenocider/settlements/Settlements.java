aa09228691caeeb204a3aea1d517229473b58136
Xenocider <xenocider.dev@gmail.com>
2014-02-24 10:04:51 -0800

    blah

---
+++ src/xenocider/settlements/Settlements.java
@@ -0,0 +1,15 @@
+package xenocider.settlements;
+
+
+import java.awt.Frame;
+
+public class Settlements extends Frame {
+
+	public static void main(String[] args) {
+		new Settlements();
+	}
+
+	Settlements(){
+	}
+
+}
